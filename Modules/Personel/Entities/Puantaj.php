c8c929d08a662dd50519ac2cfcf6a101ec5f373d
Ekrem OĞUL <ekremogul@gmail.com>
2021-11-26 17:44:49 +0300

    Puantaj QueryCache

--- Modules/Personel/Entities/Puantaj.php
+++ Modules/Personel/Entities/Puantaj.php
@@ -4,13 +4,16 @@ namespace Modules\Personel\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Puantaj extends Model
 {
     use HasFactory;
+    use QueryCacheable;
 
     protected $guarded = [];
     protected $table = "personel_puantaj";
+    protected $cacheFor = 60*60*24;
 
     protected static function newFactory()
     {
