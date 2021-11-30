743c9cb370f0eba57ce80ee4e428d0517b883897
Ekrem OĞUL <ekremogul@gmail.com>
2021-11-30 18:10:57 +0300

    ...

--- Modules/Odeme/Routes/web.php
+++ Modules/Odeme/Routes/web.php
@@ -4,4 +4,4 @@
     Route::post('/odemeal', 'OdemeController@odemeal')->name('odemeal')->middleware('auth');
     Route::match(['post', 'get'], '/odemesonuc', 'OdemeController@odemesonuc')->name('odemesonuc')->middleware('auth');
     Route::get('/odeme-basarili', 'OdemeController@success')->name('success')->middleware('auth');
-    Route::get('/odeme-basarisiz', 'OdemeControlle@failed')->name('failed')->middleware('auth');
+    Route::get('/odeme-basarisiz', 'OdemeController@failed')->name('failed')->middleware('auth');
