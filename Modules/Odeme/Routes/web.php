aa0dc753080e470a68d84766740207737ff5f7cc
yuzgec <olcayy@gmail.com>
2021-12-01 13:20:21 +0300

    Odeme Auth Düzeltme

--- Modules/Odeme/Routes/web.php
+++ Modules/Odeme/Routes/web.php
@@ -3,5 +3,5 @@
     Route::resource('/odeme', 'OdemeController')->middleware('auth');
     Route::post('/odemeal', 'OdemeController@odemeal')->name('odemeal')->middleware('auth');
     Route::match(['post', 'get'], '/odemesonuc', 'OdemeController@odemesonuc')->name('odemesonuc')->middleware('auth');
-    Route::get('/odeme-basarili', 'OdemeController@success')->name('success')->middleware('auth');
-    Route::get('/odeme-basarisiz', 'OdemeController@failed')->name('failed')->middleware('auth');
+    Route::get('/odeme-basarili', 'OdemeController@success')->name('success');
+    Route::get('/odeme-basarisiz', 'OdemeController@failed')->name('failed');
