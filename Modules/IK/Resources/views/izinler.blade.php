1acc3b3599f3bb53c7b9ccda670c3307f0f3990a
yuzgec <olcayy@gmail.com>
2021-12-27 06:20:33 +0300

    IK

--- Modules/IK/Resources/views/izinler.blade.php
+++ Modules/IK/Resources/views/izinler.blade.php
@@ -1,12 +1,27 @@
 @extends('master')
 @section('title', 'İzin Yönetimi | '.config('app.name'))
+@section('customJS')
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('a[data-bs-toggle="tab"]').on('show.bs.tab', function(e) {
+                localStorage.setItem('activeTab', $(e.target).attr('href'));
+            });
+            var activeTab = localStorage.getItem('activeTab');
+            console.log(activeTab);
+            if(activeTab){
+                $('#tab a[href="' + activeTab + '"]').tab('show');
+            }
+        });
+    </script>
+@endsection
 @section('content')
 
 
 
     <div class="col-md-12">
         <div class="card">
-            <ul class="nav nav-tabs" data-bs-toggle="tabs">
+            <ul class="nav nav-tabs" data-bs-toggle="tabs" id="tab">
                 <li class="nav-item">
                     <a href="#izinler" class="nav-link active" data-bs-toggle="tab"><span class="m-2">İzinler </span></a>
                 </li>
@@ -76,15 +91,15 @@
                                                     <td data-label="Title" >
                                                         <div>Yıllık İzin</div>
                                                     </td>
-                                                    <td class="text-muted" data-label="Role" >
+                                                    <td class="text-muted" >
                                                         4
                                                     </td>
 
-                                                    <td class="text-muted" data-label="Role" >
+                                                    <td class="text-muted" >
                                                         {{ \Carbon\Carbon::today() }}
                                                     </td>
 
-                                                    <td class="text-muted" data-label="Role" >
+                                                    <td class="text-muted" >
                                                         {{ \Carbon\Carbon::yesterday() }}
                                                     </td>
 
@@ -132,15 +147,15 @@
                                                         <td data-label="Title" >
                                                             <div>Yıllık İzin</div>
                                                         </td>
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             4
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::today() }}
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::yesterday() }}
                                                         </td>
 
@@ -173,6 +188,9 @@
 
                                                 @foreach($Personel->slice(1) as $item)
                                                     <tr>
+                                                        <td data-label="Title" >
+                                                            <div>Yıllık İzin</div>
+                                                        </td>
                                                         <td data-label="Name" >
                                                             <div class="d-flex py-1 align-items-center">
                                                                 <a href="#">
@@ -188,15 +206,15 @@
                                                         <td data-label="Title" >
                                                             <div>Yıllık İzin</div>
                                                         </td>
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             4
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::today() }}
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::yesterday() }}
                                                         </td>
 
@@ -244,15 +262,15 @@
                                                         <td data-label="Title" >
                                                             <div>Yıllık İzin</div>
                                                         </td>
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             4
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::today() }}
                                                         </td>
 
-                                                        <td class="text-muted" data-label="Role" >
+                                                        <td class="text-muted" >
                                                             {{ \Carbon\Carbon::yesterday() }}
                                                         </td>
 
@@ -272,7 +290,174 @@
                     </div>
 
                     <div class="tab-pane" id="raporlar">
-                        <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc amet, pellentesque id egestas velit sed</div>
+                        <ul class="nav nav-tabs" data-bs-toggle="tabs">
+                            <li class="nav-item">
+                                <a href="#kalanizinler" class="nav-link active" data-bs-toggle="tab">Kalan İzinler <span class="badge m-2"> 4</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#aylikizinraporu" class="nav-link" data-bs-toggle="tab">Aylık İzin raporu<span class="badge m-2"> 4</span></a>
+                            </li>
+                        </ul>
+
+                        <div class="tab-content">
+                            <div class="tab-pane active show" id="kalanizinler">
+
+                                <div class="col-12 mt-2">
+                                    <div class="card">
+                                        <div class="table-responsive">
+                                            <table class="table table-vcenter table-mobile-md card-table">
+                                                <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>İsim Soyad</th>
+                                                    <th>Kimlik Numarası</th>
+                                                    <th>Şirket</th>
+                                                    <th>Depertman</th>
+                                                    <th>Ünvan</th>
+                                                    <th>İşe Giriş</th>
+                                                    <th>Extra İzin Günleri</th>
+                                                    <th>Devir Kayıpları</th>
+                                                    <th>Toplam Hah Edilen</th>
+                                                    <th>Kullanılan</th>
+                                                    <th>Kalan</th>
+                                                    <th>Çalışma Durumu</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+
+                                                @foreach($Personel->slice(1) as $item)
+                                                    <tr>
+                                                        <td data-label="Title" >
+                                                            <div>{{$item->id}}</div>
+                                                        </td>
+                                                        <td data-label="Name" >
+                                                            <div class="d-flex py-1 align-items-center">
+                                                                <a href="#">
+                                                            <span class="avatar me-2" style="background-image: url({{'/images/personel/50/'.$item->foto}})">
+                                                                {{ ($item->foto == "") ? isim($item->adsoyad) : null }}
+                                                            </span>
+                                                                </a>
+                                                                <div class="flex-fill">
+                                                                    <div class="font-weight-medium">{{$item->adsoyad}}</div>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                        <td data-label="Title" >
+                                                            <div>Yıllık İzin</div>
+                                                        </td>
+                                                        <td class="text-muted" >
+                                                            4
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            {{ \Carbon\Carbon::today() }}
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            {{ \Carbon\Carbon::yesterday() }}
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+
+                                                        <td class="text-muted" >
+                                                            İçerik
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            <span class="badge bg-success">Aktif Çalışan </span>
+                                                        </td>
+
+                                                    </tr>
+                                                @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="tab-pane" id="aylikizinraporu">
+
+                                <div class="col-12 mt-2">
+                                    <div class="card">
+                                        <div class="table-responsive">
+                                            <table class="table table-vcenter table-mobile-md card-table">
+                                                <thead>
+                                                <tr>
+                                                    <th>İsim Soyisim</th>
+                                                    <th>İzin Türü</th>
+                                                    <th>Gün</th>
+                                                    <th>Başlangıç Tarihi</th>
+                                                    <th>Bitiş Tarihi</th>
+                                                    <th class="w-1"></th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+
+                                                @foreach($Personel->slice(1) as $item)
+                                                    <tr>
+                                                        <td data-label="Name" >
+                                                            <div class="d-flex py-1 align-items-center">
+                                                                <a href="#">
+                                                            <span class="avatar me-2" style="background-image: url({{'/images/personel/50/'.$item->foto}})">
+                                                                {{ ($item->foto == "") ? isim($item->adsoyad) : null }}
+                                                            </span>
+                                                                </a>
+                                                                <div class="flex-fill">
+                                                                    <div class="font-weight-medium">{{$item->adsoyad}}</div>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                        <td data-label="Title" >
+                                                            <div>Yıllık İzin</div>
+                                                        </td>
+                                                        <td class="text-muted" >
+                                                            4
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            {{ \Carbon\Carbon::today() }}
+                                                        </td>
+
+                                                        <td class="text-muted" >
+                                                            {{ \Carbon\Carbon::yesterday() }}
+                                                        </td>
+
+                                                    </tr>
+                                                @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
                     </div>
 
                     <div class="tab-pane" id="kurallar">
