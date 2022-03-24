b1b48b49129db5d79091988b037f8710a309c3dd
Ekrem OĞUL <ekremogul@gmail.com>
2022-03-24 14:06:56 +0300

    ...

--- Modules/CallCenter/Resources/views/dosya/index.blade.php
+++ Modules/CallCenter/Resources/views/dosya/index.blade.php
@@ -31,10 +31,24 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 13v-8a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-5.5m-9.5 -2h7m-3 -3l3 3l-3 3" /></svg>
                             Dosya Ekle
                         </a>
-                       <a class="btn" href="{{ route('grup.index') }}">
+                       <a class="btn" href="{{ route('alacakli.index') }}">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="9" cy="7" r="4" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
-                           Grup Ekle
+                           Alacaklı Yönetimi
                        </a>
+                       <a class="btn" href="{{ route('borclu.index') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="9" cy="7" r="4" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
+                           Borçlu Yönetimi
+                       </a>
+                       <a class="btn" href="javascript:;" data-bs-toggle="dropdown">
+                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-layout-list" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><rect x="4" y="4" width="16" height="6" rx="2"></rect><rect x="4" y="14" width="16" height="6" rx="2"></rect></svg>
+                           Tanımlamalar
+                       </a>
+                        <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <a href="{{route('dosya_gruplari.index')}}" class="dropdown-item">Dosya Grupları</a>
+                            <a href="{{route('foy_durumlari.index')}}" class="dropdown-item">Föy Durumları</a>
+                            <a href="{{route('form_turleri.index')}}" class="dropdown-item">Form Türleri</a>
+                            <a href="{{route('icra_mudurlugu.index')}}" class="dropdown-item">İcra Müdürlükleri</a>
+                        </div>
                     </div>
                 </div>
                 <div class="table-responsive">
